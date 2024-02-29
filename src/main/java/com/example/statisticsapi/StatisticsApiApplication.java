3b0bbebd5ec051e2136b372d8264306158c2d196
Vadym Hurnik <vad.com.z@gmail.com>
2024-02-29 14:34:45 +0200

    init commit

---
+++ src/main/java/com/example/statisticsapi/StatisticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.statisticsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/statisticsapi/StatisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.statisticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
